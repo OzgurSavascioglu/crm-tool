e9617da76f18fe86c808a50c88267b8fcb96a68c
OzgurSavascioglu <savasciogluozgur@gmail.com>
2025-08-11 13:47:36 +0300

    Initial commit

---
+++ src/test/java/ozgur/crmTool/CrmToolApplicationTests.java
@@ -0,0 +1,13 @@
+package ozgur.crmTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
